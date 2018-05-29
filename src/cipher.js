77a023a375e31fe948935a2c66e3907fcf250bb6
noelyGitHub <noely.alis209@gmail.com>
2018-05-29 12:47:14 -0500

    subiendo modificaciones sn6

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,5 @@
-window.cipher = {
-	encode: function(offset, cadena){debugger
-	const sinCifrar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+encriptar=(offset, cadena)=>{debugger
+	const sinCifrar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 		let numero1=cadena.length;//conflictos!! tama;o de mi mensaje
 		let numero2=sinCifrar.length;//conflictos!! Numero de letras-27
 		let numeroDesplazamiento=parseInt(offset);// Conflictos!!! parsear
@@ -8,7 +7,6 @@ window.cipher = {
 		let textCifrado="";//inicializando mi variable que ALMACENARA mi cifrado
 		let posicionText;// variable que calcula la pocision nueva de mis caracteres
 		for (let i = 0; i <=numero1 ; i++) {//recorrere por la cantidad de argumentos en mi mensaje	
-			//var obtenerNumero=Math.sign(letra[i]);// (1,-1,0,NaN)
 			if (!isNaN(letra[i])) {// quiero determinar si el argumento es un numero o no! (NaN->true,12->false) NO ES UN NUMERO???
 				textCifrado+=letra[i];
 			};
@@ -25,9 +23,9 @@ window.cipher = {
 				}
 			}
 		return textCifrado.toString(); 
-	}, 
-	decode: function(offset, cadena){debugger
-	const sinCifrar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	}
+desencriptar=(offset, cadena)=>{debugger
+	const sinCifrar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 		let numero1=cadena.length;//conflictos!! tama;o de mi mensaje
 		let numero2=sinCifrar.length;//conflictos!! Numero de letras-27
 		let numeroDesplazamiento=parseInt(offset);// Conflictos!!! parsear
@@ -35,7 +33,6 @@ window.cipher = {
 		let textCifrado="";//inicializando mi variable que ALMACENARA mi cifrado
 		let posicionText;// variable que calcula la pocision nueva de mis caracteres
 		for (let i = 0; i <=numero1 ; i++) {//recorrere por la cantidad de argumentos en mi mensaje	
-			//var obtenerNumero=Math.sign(letra[i]);// (1,-1,0,NaN)
 			if (!isNaN(letra[i])) {// quiero determinar si el argumento es un numero o no! (NaN->true,12->false) NO ES UN NUMERO???
 				textCifrado+=letra[i];
 			};
@@ -57,6 +54,47 @@ window.cipher = {
 				}
 			}
 		return textCifrado.toString(); 
-	} 
+	}
+window.cipher = {
+	encode: encriptar, 
+	decode: desencriptar,
+	//createCipherWithOffset: WithOffset;
 };
 
+
+WithOffset=(offset,cadena,boolean)=>{
+		const sinCifrar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+		let numero1=cadena.length;//conflictos!! tama;o de mi mensaje
+		let numero2=sinCifrar.length;//conflictos!! Numero de letras-27
+		let numeroDesplazamiento=parseInt(offset);// Conflictos!!! parsear
+		let letra=cadena.toUpperCase();
+		let textCifrado="";//inicializando mi variable que ALMACENARA mi cifrado
+		let posicionText;// variable que calcula la pocision nueva de mis caracteres
+		for (let i = 0; i <=numero1 ; i++) {//recorrere por la cantidad de argumentos en mi mensaje	
+			//var obtenerNumero=Math.sign(letra[i]);// (1,-1,0,NaN)
+			if (!isNaN(letra[i])) {// quiero determinar si el argumento es un numero o no! (NaN->true,12->false) NO ES UN NUMERO???
+				textCifrado+=letra[i];
+			};
+			for(let q=0; q<=numero2-1;q++)
+				{ 									
+					if(letra[i]!=" "){	// .........si mi letra es un espacio en blanco??..........	
+							if(letra[i]==sinCifrar[q]){// comparo cada argumento de mi mensaje con el Array abecedario
+								if(boolean==true){//si es verdadero cifra
+									posicionCifrado=(q+numeroDesplazamiento)%numero2;
+									textCifrado+=sinCifrar[posicionCifrado];
+								}else{// si es falso descifra									
+									if(posicionCifrado2<0){
+										posicionCifrado2=(((q+numeroDesplazamiento)%numero2)-numeroDesplazamiento)%numero2;
+										textCifrado+=sinCifrar[posicionCifrado2+numero2];
+									}else{
+										textCifrado+=sinCifrar[posicionCifrado2];
+									}
+								}
+							}
+					}else{
+						textCifrado+=" ";
+					}					
+				}
+			}
+		return textCifrado.toString();
+}
--- src/index.js
+++ src/index.js
@@ -3,28 +3,37 @@ window.onload=function() {
 	// body...
 }
 DOMCipherEncode=()=>{
-	var num=document.getElementById("offsetNumber");
+	var offsetNumber=document.getElementById("offsetNumber");
 	var textMessage=document.getElementById("cipherText");
 	var offsetClave=document.getElementById("offsetClave");
+	// 	ENCRIPTAR
 	document.getElementById("buttonCipherText").addEventListener("click",()=>{debugger
-		if(num.value!="" && textMessage.value!=""){
-			document.getElementById("messageCipher").style.display="none";
-			document.getElementById("messageDes").style.display="block";
-			document.getElementById("warnnig").style.display="none";			
-			document.getElementById("cipherMessage").innerHTML=cipher.encode(num.value,textMessage.value);
+		if(offsetNumber.value!="" && textMessage.value!=""){
+			document.getElementById("divCipher").style.display="none";
+			document.getElementById("divDescipher").style.display="block";
+			document.getElementById("warning").style.display="none";			
+			document.getElementById("cipherMessage").innerHTML=cipher.encode(offsetNumber.value,textMessage.value);
 		}else{
-			document.getElementById("warnnig").style.display="block";
+			document.getElementById("warning").style.display="block";
 		}
-			
-
 	});
+	// DESEMCRIPTAR
 	document.getElementById("buttonDecipherText").addEventListener("click",()=>{
-		if(offsetClave.value!="" && offsetClave.value==num.value){
-			document.getElementById("warnnig2").style.display="none";
-			document.getElementById("cipherMessage").innerHTML=cipher.decode(offsetClave.value,cipher.encode(num.value,textMessage.value));
+		if(offsetClave.value!="" && offsetClave.value==offsetNumber.value){
+			document.getElementById("warning2").style.display="none";
+			document.getElementById("cipherMessage").innerHTML=cipher.decode(offsetClave.value,cipher.encode(offsetNumber.value,textMessage.value));
 		}else{
 			document.getElementById("warnnig2").style.display="block";
 		}
 	});
+	// VALIDAR INPUT
+	offsetNumber.addEventListener("keypress",soloNumeros);
+	offsetClave.addEventListener("keypress",soloNumeros);
 
+	 function soloNumeros(e){debugger
+	 	var key = window.event ? e.which : e.keyCode;
+ 		if (key < 48 || key > 57) {
+   			 e.preventDefault();
+  			}
+	}
 } 
